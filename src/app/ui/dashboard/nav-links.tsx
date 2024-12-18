7c18d7c3c2dabb3fcc870d2ed72a0f24b5760da2
Romualdo Dasig <dasig.rg@a5project.com>
2024-12-18 21:30:59 +0800

    Add navigation links

--- src/app/ui/dashboard/nav-links.tsx
+++ src/app/ui/dashboard/nav-links.tsx
@@ -13,12 +13,14 @@ import clsx from 'clsx';
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
+  { name: 'Income', href: '/income', icon: DocumentDuplicateIcon},
+  { name: 'Expenses', href: '/expenses', icon: DocumentDuplicateIcon},
+  { name: 'Account', href: '/account', icon: DocumentDuplicateIcon},
+  { name: 'Budget', href: '/budget', icon: DocumentDuplicateIcon},
+  { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Reports', href: '/reports', icon: DocumentDuplicateIcon},
+  { name: 'Settings', href: '/settings', icon: DocumentDuplicateIcon},
 ];
 
 export default function NavLinks() {
