33d591b7eba3ec65059d538961cad367644c1f90
Romualdo Dasig <dasig.rg@a5project.com>
2025-01-01 21:55:58 +0800

    FINAPP-44 Update main menu icons

--- src/app/ui/main-menu.tsx
+++ src/app/ui/main-menu.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import Link from 'next/link'
+import { HomeIcon, PlusIcon, UserIcon, CogIcon } from '@heroicons/react/24/outline'
 import {
   Dialog,
   DialogPanel,
@@ -17,21 +18,27 @@ export default function MainMenu() {
 
   return (
     <footer className="bg-white lg:hidden">
-      <div className="fixed bottom-0 w-full bg-white bg-opacity-90 border-t text-grey-700 flex justify-around py-4 shadow-lg z-50">
+      <div className="fixed bottom-0 w-full bg-white bg-opacity-90 border-t text-grey-700 flex justify-around py-2 shadow-lg z-50">
         <Link
           href="/dashboard"
+          className="flex h-8 items-center rounded-lg px-2 text-sm font-medium text-grey-50 transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
         >
-          Home
+          <span className="hidden md:block">Create Expense</span>{' '}
+          <HomeIcon className="h-5 md:ml-4" />
         </Link>
         <Link
-          href="/dashboard/invoices"
+          href="/dashboard/expenses/create"
+          className="flex h-8 items-center rounded-lg px-2 text-sm font-medium text-grey-50 border transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
         >
-          Invoices
+          <span className="hidden md:block">Create Expense</span>{' '}
+          <PlusIcon className="h-5 md:ml-4" />
         </Link>
         <Link
-          href="/dashboard"
+          href="/settings"
+          className="flex h-8 items-center rounded-lg px-2 text-sm font-medium text-grey-50 transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
         >
-          Profile
+          <span className="hidden md:block">Create Expense</span>{' '}
+          <CogIcon className="h-5 md:ml-4" />
         </Link>
       </div>
     </footer>
