97612fde9c34d13dba8b42ad96e3cb1a217e35d6
Romualdo Dasig <dasig.rg@a5project.com>
2025-01-12 22:19:12 +0800

    FINAPP-44 Test user login

--- cypress/e2e/user/login.cy.ts
+++ cypress/e2e/user/login.cy.ts
@@ -1,19 +1,28 @@
 describe('Navigation', () => {
   beforeEach(() => {
-    cy.viewport(375, 667);
-    cy.visit('dashboard')
+    cy.viewport(375, 667)
+  })
+
+  afterEach(() => {
+    cy.wait(2000)
   })
 
   it('should navigate to the income page', () => {
-    cy.get('main').within(() => {
-      // Find a link with an href attribute containing "income" and click it
-      cy.get('a[href*="/dashboard/income"]').click()
-      
-      // The new url should include "/dashboard/account"
-      cy.url().should('include', '/dashboard/income')
+    cy.visit('/')
 
-      // The new page should contain an h1 with "Income"
-      cy.get('h1').contains('Income')
+    cy.get('main').within(() => {
+      // Click on the Log in button.
+      cy.get('a[href*="/login"]').click()
+
+      cy.get('form').within(() => {
+        // Fill in the login form.
+        cy.get('input#email').type('test-engineer@a5project.com')
+        cy.get('input#password').type('T064vYL3LkZ9sGvB')
+        cy.get('button').contains('Log in').click()
+      })
     })
+
+    // Assert that you're able to access the dashboard page.
+    cy.get('main').should('contain', 'Budget')
   })
 })
