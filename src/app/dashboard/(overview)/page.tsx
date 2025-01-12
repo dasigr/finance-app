b672bc2961a89a9f90cb1cc71b3429445973abf1
Romualdo Dasig <dasig.rg@a5project.com>
2025-01-12 18:08:17 +0800

    FINAPP-66 Be able to logout

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -1,6 +1,4 @@
-import { signOut } from '@/auth';
 import { Metadata } from 'next';
-import { PowerIcon } from '@heroicons/react/24/outline';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import { Suspense } from 'react';
 import {
@@ -21,19 +19,6 @@ export default async function Page() {
 
   return (
     <div className="pb-4 mb-12">
-      <div>
-      <form
-          action={async () => {
-            'use server';
-            await signOut();
-          }}
-        >
-          <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form>
-      </div>
       <div className="mt-4 mb-4">
         <p className="mb-4">Budget: {formatCurrency(totalBudgetAmount)}</p>
         <BudgetStatus budgetAmount={totalBudgetAmount} />
