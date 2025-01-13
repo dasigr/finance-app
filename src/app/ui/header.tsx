3bf541dea686eadecd9d230c1cc62a23e3030ce0
Romualdo Dasig <dasig.rg@a5project.com>
2025-01-13 22:02:46 +0800

    FINAPP-45 Display header and update main menu

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -11,36 +11,45 @@ import {
 import {
   Bars3Icon,
   XMarkIcon,
+  UserIcon,
+  BellIcon,
+  ArrowRightStartOnRectangleIcon,
 } from '@heroicons/react/24/outline'
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-white hidden sm:block">
-      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+    <header className="bg-white sm:block">
+      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between py-4 px-6 lg:px-8">
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 mr-8"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="size-6" />
+          </button>
+        </div>
         <div className="flex lg:flex-1">
           <Link 
-            href="/dashboard"
+            href="/"
             className="-m-1.5 p-1.5"
           >
             <span className="sr-only">Finance</span>
             <Image
               src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+              width={1000}
+              height={760}
               alt="Tailwind CSS"
               className="h-8 w-auto"
             />
           </Link>
         </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="size-6" />
-          </button>
+        <div className="flex justify-center gap-4 lg:hidden">
+          <BellIcon aria-hidden="true" className="size-6" />
+          <ArrowRightStartOnRectangleIcon aria-hidden="true" className="size-6" />
         </div>
         <PopoverGroup className="hidden lg:flex lg:gap-x-12">
           <Link
@@ -91,24 +100,30 @@ export default function Header() {
       </nav>
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
         <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-4 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <Link href="/dashboard" className="-m-1.5 p-1.5">
-              <span className="sr-only">Finance</span>
-              <Image
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Tailwind CSS"
-                className="h-8 w-auto"
-              />
-            </Link>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700 mr-8"
             >
               <span className="sr-only">Close menu</span>
               <XMarkIcon aria-hidden="true" className="size-6" />
             </button>
+            <Link href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">Finance</span>
+              <Image
+                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                alt="Tailwind CSS"
+                width={1000}
+                height={760}
+                className="h-8 w-auto"
+              />
+            </Link>
+            <div className="flex justify-center gap-4">
+              <BellIcon aria-hidden="true" className="size-6" />
+              <ArrowRightStartOnRectangleIcon  aria-hidden="true" className="size-6" />
+            </div>
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
@@ -156,6 +171,12 @@ export default function Header() {
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
                   Log in
+                </Link>
+                <Link
+                  href="/logout"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Log out
                 </Link>
               </div>
             </div>
