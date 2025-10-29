a4dbf7a7c293823a2400f9b9131bab6eae4d0b91
Romualdo Dasig <dasig.rg@a5project.com>
2025-10-29 22:10:43 +0800

    Configure PWA

--- src/app/actions/pwa.ts
+++ src/app/actions/pwa.ts
@@ -31,7 +31,7 @@ export async function sendNotification(message: string) {
 
   try {
     await webpush.sendNotification(
-      subscription,
+      subscription as any,
       JSON.stringify({
         title: 'Test Notification',
         body: message,
