b5e63cf37576dbcafe4ff298f2d06933669235a4
Romualdo Dasig <dasig.rg@a5project.com>
2025-12-13 12:39:26 +0800

    Seed incomes

--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -382,7 +382,7 @@ export async function GET() {
     // await seedExpenses();
 
     await seedIncomeCategories();
-    await seedIncomes();
+    // await seedIncomes();
     
     // await seedLedgers();
     // await seedTransactions();
