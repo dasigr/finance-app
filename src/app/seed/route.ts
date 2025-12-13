343debea150f8d50558c1973a1cd5747ea1b40da
Romualdo Dasig <dasig.rg@a5project.com>
2025-12-13 09:22:53 +0800

    Seed all

--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -373,12 +373,15 @@ export async function GET() {
     // await seedUsers();
     // await seedCustomers();
     // await seedInvoices();
+    
     await seedRevenue();
     await seedExpenseCategories();
     await seedBudget();
-    await seedAccounts();
-    await seedExpenses();
+
+    // await seedAccounts();
+    // await seedExpenses();
     // await seedIncomeCategories();
+    
     // await seedIncomes();
     // await seedLedgers();
     // await seedTransactions();
