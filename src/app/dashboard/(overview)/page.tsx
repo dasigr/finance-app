2f4debaa5b68e8d524cd6e410cccafdf675c6ae6
Romualdo Dasig <dasig.rg@a5project.com>
2025-12-20 14:17:04 +0800

    Display goals

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -8,6 +8,10 @@ import { formatCurrency } from '@/app/lib/utils'
 import { BudgetStatus } from '@/app/components/budget/budget-status'
 import { fetchTotalBudgetAmount } from '@/app/lib/actions/budget'
 
+import Link from 'next/link'
+import { TrophyIcon } from '@heroicons/react/24/outline'
+import { lusitana } from '@/app/fonts'
+
 export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
@@ -15,7 +19,39 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  const totalBudgetAmount = await fetchTotalBudgetAmount();
+  const totalBudgetAmount = await fetchTotalBudgetAmount()
+  const goals:any = [
+    {
+      name: 'Retirement',
+      amount: '1500000000',
+      status: 'In Progress'
+    },
+    {
+      name: 'Emergency Fund',
+      amount: '30000000',
+      status: 'In Progress'
+    },
+    {
+      name: 'Debt',
+      amount: '0',
+      status: 'In Progress'
+    },
+    {
+      name: 'Car (Minivan)',
+      amount: '24000000',
+      status: 'To Do'
+    },
+    {
+      name: 'House and Lot',
+      amount: '190000000',
+      status: 'Completed'
+    },
+    {
+      name: 'Motorcycle (XRM 125)',
+      amount: '8390000',
+      status: 'Completed'
+    }
+  ]
 
   return (
     <div className="pb-4 mb-12">
@@ -23,10 +59,53 @@ export default async function Page() {
         <p className="mb-4">Budget: {formatCurrency(totalBudgetAmount)}</p>
         <BudgetStatus budgetAmount={totalBudgetAmount} />
       </div>
-      <div className="grid gap-2 grid-cols-2 sm:grid-cols-2 lg:grid-cols-4">
+      <div className="mb-4 grid gap-2 grid-cols-2 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
+      </div>
+      <div className="mb-4 grid gap-2 grid-cols-1">
+        <div className={`rounded-xl p-1 shadow-sm bg-slate-400`}>
+          <Link href={`/dashboard/goals`} className="">
+            <div className="flex pt-2 px-2">
+              <TrophyIcon className="h-5 w-5 text-white" />
+              <h3 className="ml-2 text-sm text-white font-medium">Goals</h3>
+            </div>
+            <p className={`${lusitana.className} truncate rounded-xl px-2 py-2 text-xl text-white`}>
+              ₱15,000,000.00
+            </p>
+            <table className="min-w-full bg-gray-200 md:table mb-2">
+              <thead className="text-left text-sm font-normal">
+                <tr>
+                  <th scope="col" className="p-2 font-bold sm:pl-6">
+                    Goal
+                  </th>
+                  <th scope="col" className="p-2 font-bold">
+                    Amount
+                  </th>
+                  <th scope="col" className="p-2 font-bold">
+                    Status
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {goals?.map((goal: any) => (
+                  <tr className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
+                    <td className="whitespace-nowrap p-2">
+                      {goal.name}
+                    </td>
+                    <td className="whitespace-nowrap p-2">
+                      {formatCurrency(goal.amount)}
+                    </td>
+                    <td className="whitespace-nowrap p-2">
+                      {goal.status}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </Link>
+        </div>
       </div>
     </div>
   )
