184b2c890d7b792ceea4b3be4e6a67a6a357bc87
Romualdo Dasig <dasig.rg@a5project.com>
2026-01-14 19:55:51 +0800

    Disable request to FastForex API

--- src/app/api/forex/route.ts
+++ src/app/api/forex/route.ts
@@ -1,16 +1,22 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const url = `https://api.fastforex.io/fetch-one?from=USD&to=PHP`;
+  // const url = `https://api.fastforex.io/fetch-one?from=USD&to=PHP`;
 
-  const res = await fetch(url, {
-    headers: {
-      "X-API-Key": `${process.env.FAST_FOREX_API_KEY}`,
-    },
-    next: { revalidate: 60 }, // cache for 1 minute
-  });
+  // const res = await fetch(url, {
+  //   headers: {
+  //     "X-API-Key": `${process.env.FAST_FOREX_API_KEY}`,
+  //   },
+  //   next: { revalidate: 60 }, // cache for 1 minute
+  // });
 
-  const data = await res.json();
+  // const data = await res.json();
+
+  const data = {
+    "result": {
+      "PHP": 59.52
+    }
+  }
 
   return NextResponse.json(data);
 }
